efb89075cff3867400fb6714705245b24128c41c
ProfL3403 <ProfL3403@lab.aca>
2022-11-18 10:16:40 -0500

    Permisos para localizacion implementados

--- app/src/main/java/pe/edu/ulima/pm/demoextrasapp/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/demoextrasapp/MainActivity.kt
@@ -7,12 +7,17 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_MUTABLE
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import pe.edu.ulima.pm.demoextrasapp.presentation.MainScreen
 
 val CHANNEL_ID = "1"
@@ -22,6 +27,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestLocationPermission()
 
         createNotificationChannel()
 
@@ -91,4 +98,48 @@ class MainActivity : ComponentActivity() {
     }
 
     // Localizacion
+
+    private val requestPermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { seOtorgaronPermisos ->
+        if (seOtorgaronPermisos) {
+            Log.i("Location", "Se otorgaron los permisos")
+        } else {
+            Log.i("Location", "NO se otorgaron los permisos")
+        }
+    }
+
+    private fun requestLocationPermission() {
+        when {
+            // 1. El usuario ya dio los permisos antes (para todas las veces)
+            ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Location", "El usuario ya dio permisos")
+            }
+
+            // 2. No dio los permisos y se necesitan
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                finish()
+            }
+
+            // 3. Aun no le ha dado los permisos
+            else -> {
+                // Lanzar pantalla para pedir permisos
+                requestPermissionsLauncher.launch(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
